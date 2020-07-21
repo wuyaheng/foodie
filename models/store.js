f6bb537ab0ec900a7046a5fd7aae41231299b37d
wuyaheng <wuyaheng2016@gmail.com>
2020-07-21 12:28:40 -0400

    define table

---
+++ models/store.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const storeSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String
+    },
+    street: {
+        type: String
+    },
+    city: {
+        type: String
+    },
+    state: {
+        type: String
+    },
+    zipcode: {
+        type: String
+    },
+    image: {
+        type: String
+    },
+    url: {
+        type: String
+    }
+});
+
+const Store = mongoose.model("Store", storeSchema);
+
+module.exports = Store;  
