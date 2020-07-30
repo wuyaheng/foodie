f8a5e682eea98492b4c2eb14d47feb77e791b069
wuyaheng <wuyaheng2016@gmail.com>
2020-07-30 18:05:58 -0400

    add grid to card

--- client/src/components/Store/index.js
+++ client/src/components/Store/index.js
@@ -8,23 +8,25 @@ function Store( { name, phone, street, city, state, zip_code, image, rating, url
 
     return (
         <ListItem> 
-        <Row className="text-center">
-            <h3>{name}</h3>
-                <div><img style={{objectFit: "cover", height:"300px"}} className="img-thumbnail img-fluid w-100" src={image} alt={name}/></div>
+        <Row>
+            <h3 style={{margin: "0 auto"}}>{name}</h3>
+                <div><img style={{objectFit: "cover", height:"300px"}} className="img-thumbnail img-fluid w-100" src={image} alt={name}/>
                
-                <p style={{display:"block"}}><i class="fas fa-comments"></i> Rating: {rating}</p>
+                <p className="text-left"><i class="fas fa-comments"></i> Rating: {rating}</p>
                
-                {phone && <p><i class="fas fa-phone"></i> {phone}</p>} 
+                {phone && <p className="text-left"><i class="fas fa-phone"></i> {phone}</p>}
              
-                <p className="small"><i class="fas fa-map-marker-alt"></i> {street}, {city}, {state} {zip_code}</p>
-               
+                <p className="small text-left"><i class="fas fa-map-marker-alt"></i> {street}, {city}, {state} {zip_code}</p>
+            </div> 
             </Row>
-            <Row className="flex-wrap-reverse">
+            <Row>
                     <div className="btn-container">
+                   
                         <a className="btn btn-light" target="_blank" rel="noopener noreferrer" href={url}>
                             View
                         </a>
                         <Button />
+                       
                     </div>
                 
             </Row>
